8f622f52f45be6b6a1182ca4da1610dc47817b0e
Mohit Mali <mohitasdeveloper@gmail.com>
2026-08-18 17:57:46 +0530

    Refactor Supabase client initialization

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,4 +1,7 @@
 const SUPABASE_URL = "https://jlabpujctouvunxpvtny.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_20aNqithPS-wbJ86F4jz1Q_0JivBCiQ";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// CDN exposes it as `supabase` object with createClient method
+const { createClient } = supabase;
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
